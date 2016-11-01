168cd4d665ce9e22029aeae0191da07922e033e3
Chrissweir <christopher.weir@hotmail.com>
2016-11-01 18:18:44 +0000

    Updates

--- Math Game/Math Game/View/PlayNormal.xaml.cs
+++ Math Game/Math Game/View/PlayNormal.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,8 +24,60 @@ namespace Math_Game.View
     /// </summary>
     public sealed partial class PlayNormal : Page
     {
-        private int staticNumA, staticNumB, staticResult, staticRandomResult;
+        private int staticNumA, staticNumB, staticResult, staticRandomResult,Score=0,State=1,BestScore=0,mode;
         private DispatcherTimer dispatcherTimer;
+
+        private void setupProgressBar()
+        {
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += DispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 1);
+            dispatcherTimer.Start();
+
+            progressBar.Value = 9999;
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            Windows.UI.Core.SystemNavigationManager.GetForCurrentView().BackRequested -= PlayNormal_BackRequested;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Windows.UI.Core.SystemNavigationManager.GetForCurrentView().BackRequested += PlayNormal_BackRequested;
+            dispatcherTimer = null;
+        }
+
+        private async void PlayNormal_BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            e.Handled = true;
+            dispatcherTimer.Stop();
+            dispatcherTimer = null;
+
+            var msg = new MessageDialog("Do you want to Stop?");
+            var okBtn = new UICommand("Yes");
+            var cancelBtn = new UICommand("No");
+            msg.Commands.Add(okBtn);
+            msg.Commands.Add(cancelBtn);
+            IUICommand result = await msg.ShowAsync();
+
+            if (result != null && result.Label.Equals("Yes"))
+            {
+                Frame.Navigate(typeof(GameOver),Score.ToString()); //Navigate to gameover page and send the score
+            }
+        }
+
+        private void DispatcherTimer_Tick(object sender, object e)
+        {
+            progressBar.Value -= Common.Common.Speed;
+            if(progressBar.Value <= 0)
+            {
+                dispatcherTimer.Stop();
+                dispatcherTimer = null;
+
+                Frame.Navigate(typeof(GameOver), Score.ToString());
+            }
+        }
 
         public PlayNormal()
         {
@@ -32,7 +86,21 @@ namespace Math_Game.View
 
         private void btnTrue_Click(object sender, RoutedEventArgs e)
         {
+            if(mode == 1) // mode - 1 so correct answer is true
+            {
+                txtScore.Text = String.Format("Score:{0}".ToUpper(), ++Score);
+                txtState.Text = String.Format("{0}", ++State);
+                dispatcherTimer.Stop();
+                dispatcherTimer = null;
+                Playing();
+            }
+            else
+            {
+                dispatcherTimer.Stop();
+                dispatcherTimer = null;
 
+                Frame.Navigate(typeof(GameOver), Score.ToString());
+            }
         }
     }
 }
