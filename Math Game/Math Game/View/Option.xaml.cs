6c9b7376a9f3dc795c73e94ecd8bd273637a7bd3
Chrissweir <christopher.weir@hotmail.com>
2016-11-03 21:04:28 +0000

    Updates and Added Options page

--- Math Game/Math Game/View/Option.xaml.cs
+++ Math Game/Math Game/View/Option.xaml.cs
@@ -20,21 +20,11 @@ namespace Math_Game.View
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class GameOver : Page
+    public sealed partial class Option : Page
     {
-        public GameOver()
+        public Option()
         {
             this.InitializeComponent();
-        }
-
-        private void btnTryAgain_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btnHome_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
